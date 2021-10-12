9149852997ec149febbabedf7622c30a0c021271
AlexLangman <alulang@hotmail.com>
2021-10-12 11:03:31 -0300

    agregando correcciones

--- script.sql
+++ script.sql
@@ -42,17 +42,15 @@ CREATE TABLE notas (
   descripcion VARCHAR(100) not null,
   eliminar INT NOT NULL DEFAULT 1,
   id_usuario INT NULL,
-  id_categoria INT NULL,
-  --CONSTRAINT `id_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`));
-  --CONSTRAINT `id_categorias` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`));
+  FOREIGN KEY(id_usuario) REFERENCES usuarios(id));
   
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 1","Descripcion nota 1",1,5);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 2","Descripcion nota 2",2,6);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 3","Descripcion nota 3",4,6);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 4","Descripcion nota 4",4,3);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 5","Descripcion nota 5",7,7);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 6","Descripcion nota 6",8,1);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 7","Descripcion nota 7",1,2);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 8","Descripcion nota 8",2,9);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 9","Descripcion nota 9",9,8);
-INSERT INTO notas (titulo,descripcion,id_usuario,id_categoria) values ("Nota 10","Descripcion nota 10",2,8)
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 1","Descripcion nota 1",1);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 2","Descripcion nota 2",2);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 3","Descripcion nota 3",4);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 4","Descripcion nota 4",4);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 5","Descripcion nota 5",7);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 6","Descripcion nota 6",8);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 7","Descripcion nota 7",1);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 8","Descripcion nota 8",2);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 9","Descripcion nota 9",9);
+INSERT INTO notas (titulo,descripcion,id_usuario) values ("Nota 10","Descripcion nota 10",2);
